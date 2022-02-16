70ae69330d0aba7f0c60432670e4af85cadd8586
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-16 16:21:53 -0300

    Tratamento efetuado para UncheckedException

--- src/UncheckedException.java
+++ src/UncheckedException.java
@@ -3,12 +3,31 @@ import javax.swing.*;
 //Fazer a divisão de 2 valores inteiros
 public class UncheckedException {
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador: ");
 
-        int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
-        System.out.println("Resultado: " + resultado);
+        boolean continueLooping = true;
 
+        do{
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try{
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continueLooping = false;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Entrada inválida, verifique: " + e.getMessage());
+                e.printStackTrace();
+            }
+            catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Não é possível dividir por zero: " + e.getMessage());
+            }
+            finally {
+                System.out.println("Chamou finally");
+            }
+        } while(continueLooping);
+
+        System.out.println("Passou do try");
     }
 
     public static int dividir(int a, int b) {
