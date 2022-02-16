288e789bda81a82808fef88fd9b3ea38e70744c1
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2022-02-16 15:47:06 -0300

    Adicionada UncheckedException

---
+++ src/UncheckedException.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+//Fazer a divisão de 2 valores inteiros
+public class UncheckedException {
+    public static void main(String[] args) {
+        String a = JOptionPane.showInputDialog("Numerador: ");
+        String b = JOptionPane.showInputDialog("Denominador: ");
+
+        int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+        System.out.println("Resultado: " + resultado);
+
+    }
+
+    public static int dividir(int a, int b) {
+        return a / b;
+    }
+}
